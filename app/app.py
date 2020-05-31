fceca124f0dce674f6c7e2b8dd016290ba2b32db
Marek Kava <marek.kava@kavalab.pl>
2020-06-01 00:09:05 +0200

    style: add Bootstrap

--- app/app.py
+++ app/app.py
@@ -3,15 +3,18 @@ import sys
 from flask import Flask, render_template
 sys.path.append("models")
 from AccuracyScore import getAccuracyScore
-import pickle
  
 app = Flask(__name__)
  
 
 @app.route('/')
 def index():
-    
     return render_template('index.html', message=getAccuracyScore())
  
+@app.errorhandler(404)
+def page_not_found(e):
+    
+    return render_template('404.html', title="404 - Flask project"), 404
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
